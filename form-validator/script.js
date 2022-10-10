141dc150a2c361347d4a7851eaca014b93d504d2
Cem Mandraci <cemmandraci@hotmail.com>
2022-10-10 14:35:46 +0300

    Other validation rules

--- form-validator/script.js
+++ form-validator/script.js
@@ -38,11 +38,41 @@ function checkRequired(inputs) {
 }
 
 
+function checkLength(input, min, max) {
+    if(input.value.length < min) {
+        error(input, `${input.id} must be at least ${min} characters. `)
+    }else if(input.value.length > max){
+        error(input, `${input.id} must be a maximum of ${max} characters. `)
+    }else{
+        success(input);
+    }
+}
+
+function checkPasswords(input1, input2) {
+    if(input1.value !== input2.value){
+        error(input2, "Passwords not match.")
+    }
+}
+
+
+function checkPhone(input) {
+    var exp = /^\d{10}$/;
+    if(!exp.test(input.value)){
+        error(input, "Your phone must be 10 numeric character.")
+    } else{
+        success(input);
+    }
+        
+}
+
 form.addEventListener("submit", function(e){
 
     e.preventDefault();
 
-    checkRequired([username,email,password,repassword]);
+    checkRequired([username,email,password,repassword, phone]);
     checkEmail(email);
-
+    checkLength(username,7,15);
+    checkLength(password,7,12);
+    checkPasswords(password,repassword);
+    checkPhone(phone);
 });
