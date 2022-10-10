38363d4bcd3d31ba18b3540eeb06e37cf48cf0ea
Cem Mandraci <cemmandraci@hotmail.com>
2022-10-10 12:37:39 +0300

    Slider Controls

--- slider/script.js
+++ slider/script.js
@@ -20,11 +20,49 @@ var models = [
 
 
 
-var index = 2;
+var index = 0;
+var slideCount = models.length;
+
+showSlide(index);
+
+document.querySelector(".fa-circle-arrow-left").addEventListener('click',function(){
+    index--;
+    showSlide(index);
+});
 
 
-document.querySelector('.card-title').textContent = models[index].name;
+document.querySelector(".fa-circle-arrow-right").addEventListener('click',function(){
+    index++;
+    showSlide(index);
+});
 
-document.querySelector('.card-img-top').setAttribute('src', models[index].image);
 
-document.querySelector('.card-link').setAttribute('href', models[index].link);
+function showSlide(i){
+
+    // =========================== Last and first slide 
+    // ===========================
+    index = i;
+
+    if( i<0 ) {
+        index = slideCount - 1;
+    }
+    if( i>= slideCount) {
+        index = 0;
+    }
+
+
+
+    // =========================== Loading title-link-image 
+    // ===========================
+    document.querySelector('.card-title').textContent = models[index].name;
+
+    document.querySelector('.card-img-top').setAttribute('src', models[index].image);
+
+    document.querySelector('.card-link').setAttribute('href', models[index].link);
+
+}
+
+
+
+
+
